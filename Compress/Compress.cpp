49667f09f2e63ef99ca888b9fbc550ce210a6262
ForrestMarkX <forrestmarkx@outlook.com>
2023-03-02 01:52:45 -0600

    Added support for compressing files

---
+++ Compress/Compress.cpp
@@ -0,0 +1,224 @@
+// Compress.cpp : Defines the entry point for the console application.
+//
+
+/*-----------------------------------------------------------------------------
+	Global variables.
+-----------------------------------------------------------------------------*/
+
+#include "Core.h"
+#include "FCodec.h"
+
+// General.
+#if _MSC_VER
+extern "C" {HINSTANCE hInstance; }
+#endif
+extern "C" {TCHAR GPackage[64] = TEXT("UDKCompress"); }
+
+// Log.
+#include "FOutputDeviceFile.h"
+FOutputDeviceFile Log;
+
+// Error.
+#include "FOutputDeviceWindowsError.h"
+FOutputDeviceWindowsError Error;
+
+// Feedback.
+#include "FFeedbackContextAnsi.h"
+FFeedbackContextAnsi Warn;
+
+// File manager.
+#if WIN32
+#include "FFileManagerWindows.h"
+FFileManagerWindows FileManager;
+
+#if 1
+#include "FMallocAnsi.h"
+FMallocAnsi Malloc;
+#else
+#include "FMallocWindows.h"
+FMallocWindows Malloc;
+#endif
+
+#elif __PSX2_EE__
+#include "FFileManagerPSX2.h"
+FFileManagerPSX2 FileManager;
+#include "FMallocAnsi.h"
+FMallocAnsi Malloc;
+#elif __GNUG__
+#include "FFileManagerLinux.h"
+FFileManagerLinux FileManager;
+#include "FMallocAnsi.h"
+FMallocAnsi Malloc;
+#else
+#include "FFileManagerAnsi.h"
+FFileManagerAnsi FileManager;
+#endif
+
+void CompressFile(TCHAR* File)
+{
+    guard(CompressFile);
+    FString FileName = FString(File);
+    
+    GWarn->Logf(NAME_Heading, TEXT("Compressing %s"), *FileName);
+
+    FString CompressFile = FileName + TEXT(".uz2");
+    INT FileSize = GFileManager->FileSize(*FileName);
+
+    FArchive* CFileAr = GFileManager->CreateFileReader(*FileName);
+    if (!CFileAr)
+    {
+        GWarn->Logf(NAME_Heading, TEXT("Failed to read %s!"), *FileName);
+        return;
+    }
+        
+    FArchive* UFileAr = GFileManager->CreateFileWriter(*CompressFile);
+    if (!UFileAr)
+    {
+        GWarn->Logf(NAME_Heading, TEXT("Failed to write %s!"), *CompressFile);
+        return;
+    }
+
+    INT iSig = 0x0000162E;
+    FString FSig = TEXT("bz2");
+    *UFileAr << iSig;
+    *UFileAr << FSig;
+
+    FCodecFull Codec;
+    Codec.AddCodec(new FCodecRLE);
+    Codec.AddCodec(new FCodecBWT);
+    Codec.AddCodec(new FCodecMTF);
+    Codec.AddCodec(new FCodecRLE);
+    Codec.AddCodec(new FCodecHuffman);
+    Codec.Encode(*CFileAr, *UFileAr);
+
+    delete CFileAr;
+    delete UFileAr;
+    
+    INT CompressedFileSize = GFileManager->FileSize(*CompressFile);
+    GWarn->Logf(NAME_Heading, TEXT("Compressed %s -> %s (-%d%%)\n\n"), *FileName, *CompressFile, static_cast<int>((1.f - (static_cast<float>(CompressedFileSize) / static_cast<float>(FileSize))) * 100));
+    
+    unguard;
+}
+
+void DecompressFile(TCHAR* File)
+{
+    guard(DecompressFile);
+    FString FileName = FString(File);
+    
+    GWarn->Logf(NAME_Heading, TEXT("Decompressing %s"), *FileName);
+
+    FString UncompressFile = FileName.FStringReplace(TEXT(".uz2"), TEXT(""));
+    INT FileSize = GFileManager->FileSize(*FileName);
+
+    FArchive* CFileAr = GFileManager->CreateFileReader(*FileName);
+    if (!CFileAr)
+    {
+        GWarn->Logf(NAME_Heading, TEXT("Failed to read %s!"), *FileName);
+        return;
+    }
+        
+    FArchive* UFileAr = GFileManager->CreateFileWriter(*UncompressFile);
+    if (!UFileAr)
+    {
+        GWarn->Logf(NAME_Heading, TEXT("Failed to write %s!"), *UncompressFile);
+        return;
+    }
+
+    INT Signature;
+    FString OrigFilename;
+
+    *CFileAr << Signature;
+    *CFileAr << OrigFilename;
+
+    FCodecFull Codec;
+    Codec.AddCodec(new FCodecRLE);
+    Codec.AddCodec(new FCodecBWT);
+    Codec.AddCodec(new FCodecMTF);
+    Codec.AddCodec(new FCodecRLE);
+    Codec.AddCodec(new FCodecHuffman);
+    Codec.Decode(*CFileAr, *UFileAr);
+
+    delete CFileAr;
+    delete UFileAr;
+    
+    INT UncompressedFileSize = GFileManager->FileSize(*UncompressFile);
+    GWarn->Logf(NAME_Heading, TEXT("Decompressed %s -> %s (%d%%)\n\n"), *FileName, *UncompressFile, static_cast<int>((1.f - (static_cast<float>(UncompressedFileSize) / static_cast<float>(FileSize))) * 100));
+    
+    unguard;
+}
+
+void CompressMain(const TArray<TCHAR*> CmdLine)
+{
+	guard(CompressMain);
+	for( INT i = 0; i < CmdLine.Num(); i++ )
+	{
+        const FString FileExt = FString(appFExt(CmdLine(i)));
+        if( FileExt != TEXT("uz2") )
+            CompressFile(CmdLine(i));
+        else if( FileExt == TEXT("uz2") )
+            DecompressFile(CmdLine(i));
+	}
+	unguard;
+}
+
+int main(int argc, char* argv[])
+{
+	// stijn: needs to be initialized early now because we use FStrings everywhere
+	Malloc.Init();
+	GMalloc = &Malloc;
+	
+	INT ErrorLevel = 0;
+	GIsStarted = 1;
+
+#ifndef _DEBUG
+	try
+#endif
+	{
+		GIsGuarded = 1;
+
+#if !_MSC_VER
+		// Set module name.
+		//appStrcpy( GModule, "ucc" );
+		// GModule < PATH_MAX
+		appStrncpy(GModule, ANSI_TO_TCHAR(argv[0]), ARRAY_COUNT(GModule));
+#endif
+        if( argc > 1 )
+        {
+            // Parse command line.
+            TArray<TCHAR*> CmdLine;
+
+            // Init engine core.
+            appInit(TEXT("UDKCompress"), GetCommandLine(), &Malloc, &Log, &Error, &Warn, &FileManager, FConfigCacheIni::Factory, 1);
+            
+            for( INT i = 1; i < argc; i++ )
+                CmdLine.AddItem(ANSI_TO_TCHAR(argv[i]));
+            
+            CompressMain(CmdLine);
+            appPreExit();
+            GIsGuarded = 0;
+        }
+        else
+        {
+            Warn.Log(NAME_Title, TEXT("Critical Warning!"));
+            appPreExit();
+            GIsGuarded = 0;
+            wprintf(TEXT("Critical Warning: Must pass a argument!\n\n"));
+        }
+	}
+#ifndef _DEBUG
+	catch( ... )
+	{
+		Warn.Log(NAME_Title, TEXT("Critical Error!"));
+
+		// Crashed.
+		ErrorLevel = 1;
+		GIsGuarded = 0;
+		Error.HandleError();
+		wprintf(TEXT("Critical Error: %ls\n\n"), GErrorHist);
+	}
+#endif
+	appExit();
+	GIsStarted = 0;
+	system("pause");
+	return ErrorLevel;
+}
--- Core/Inc/FCodec.h
+++ Core/Inc/FCodec.h
@@ -1,34 +1,21 @@
 /*=============================================================================
-	FCodec.h: Data compression codecs.
-	Copyright 1997-1999 Epic Games, Inc. All Rights Reserved.
-
-Revision history:
-	* Created by Tim Sweeney.
+	UnCodec.h: Data compression codecs.
+	Copyright 1998-2010 Epic Games, Inc. All Rights Reserved.
 =============================================================================*/
-
-#ifndef FCODEC_H
-#define FCODEC_H
-
-#if ((_MSC_VER) || (HAVE_PRAGMA_PACK))
-#pragma pack (push,OBJECT_ALIGNMENT)
-#endif
 
 /*-----------------------------------------------------------------------------
 	Coder/decoder base class.
 -----------------------------------------------------------------------------*/
-
-class FCodecNotify
-{
-public:
-	virtual UBOOL NotifyProgress( FLOAT Progress ) { return 1; }
-};
+#ifndef FCODEC_H
+#define FCODEC_H
 
 class FCodec
 {
 public:
-	virtual ~FCodec() noexcept(false) {}
-	virtual UBOOL Encode( FArchive& In, FArchive& Out, FCodecNotify* Notify=NULL ) _VF_BASE_RET(0);
-	virtual UBOOL Decode( FArchive& In, FArchive& Out, FCodecNotify* Notify=NULL ) _VF_BASE_RET(0);
+	virtual UBOOL Encode(FArchive& In, FArchive& Out) = 0;
+	virtual UBOOL Decode(FArchive& In, FArchive& Out) = 0;
+	virtual FString GetCompressionName() = 0;
+	virtual ~FCodec() {}
 };
 
 /*-----------------------------------------------------------------------------
@@ -38,88 +25,89 @@ public:
 class FCodecBWT : public FCodec
 {
 private:
-	enum {MAX_BUFFER_SIZE=0x40000}; /* Hand tuning suggests this is an ideal size */
-	CORE_API static BYTE* CompressBuffer;
-	CORE_API static INT CompressLength;
-	static INT ClampedBufferCompare( const INT* P1, const INT* P2 )
+	enum { MAX_BUFFER_SIZE = 0x40000 }; /* Hand tuning suggests this is an ideal size */
+	static BYTE* CompressBuffer;
+	static INT CompressLength;
+	static INT ClampedBufferCompare(const INT* P1, const INT* P2)
 	{
 		guardSlow(FCodecBWT::ClampedBufferCompare);
 		BYTE* B1 = CompressBuffer + *P1;
 		BYTE* B2 = CompressBuffer + *P2;
-		for( INT Count=CompressLength-Max(*P1,*P2); Count>0; Count--,B1++,B2++ )
+		for (INT Count = CompressLength - Max(*P1, *P2); Count > 0; Count--, B1++, B2++)
 		{
-			if( *B1 < *B2 )
+			if (*B1 < *B2)
 				return -1;
-			else if( *B1 > *B2 )
+			else if (*B1 > *B2)
 				return 1;
 		}
 		return *P1 - *P2;
 		unguardSlow;
 	}
 public:
-	UBOOL Encode( FArchive& In, FArchive& Out, FCodecNotify* Notify=NULL )
+	FString GetCompressionName()
+	{
+		guard(FCodecBWT::GetCompressionName);
+		return TEXT("Burrows-Wheeler");
+		unguard;
+	}
+
+	UBOOL Encode(FArchive& In, FArchive& Out)
 	{
 		guard(FCodecBWT::Encode);
 		TArray<BYTE> CompressBufferArray(MAX_BUFFER_SIZE);
-		TArray<INT>  CompressPosition   (MAX_BUFFER_SIZE+1);
+		TArray<INT>  CompressPosition(MAX_BUFFER_SIZE + 1);
 		CompressBuffer = &CompressBufferArray(0);
-		INT i, First=0, Last=0;
-		while( !In.AtEnd() )
+		INT i, First = 0, Last = 0;
+		while (!In.AtEnd())
 		{
-			CompressLength = Min<INT>( In.TotalSize()-In.Tell(), MAX_BUFFER_SIZE );
-			In.Serialize( CompressBuffer, CompressLength );
-			for( i=0; i<CompressLength+1; i++ )
+			CompressLength = Min<INT>(In.TotalSize() - In.Tell(), MAX_BUFFER_SIZE);
+			In.Serialize(CompressBuffer, CompressLength);
+			for (i = 0; i < CompressLength + 1; i++)
 				CompressPosition(i) = i;
-			appQsort( &CompressPosition(0), CompressLength+1, sizeof(INT), (QSORT_COMPARE)ClampedBufferCompare );
-			for( i=0; i<CompressLength+1; i++ )
-				if( CompressPosition(i)==1 )
+			appQsort(&CompressPosition(0), CompressLength + 1, sizeof(INT), (QSORT_COMPARE)ClampedBufferCompare);
+			for (i = 0; i < CompressLength + 1; i++)
+				if (CompressPosition(i) == 1)
 					First = i;
-				else if( CompressPosition(i)==0 )
+				else if (CompressPosition(i) == 0)
 					Last = i;
 			Out << CompressLength << First << Last;
-			for( i=0; i<CompressLength+1; i++ )
-				Out << CompressBuffer[CompressPosition(i)?CompressPosition(i)-1:0];
-			//GWarn->Logf(TEXT("Compression table"));
-			//for( i=0; i<CompressLength+1; i++ )
-			//	GWarn->Logf(TEXT("    %03i: %ls"),CompressPosition(i)?CompressBuffer[CompressPosition(i)-1]:-1,appFromAnsi((ANSICHAR*)CompressBuffer+CompressPosition(i)));
+			for (i = 0; i < CompressLength + 1; i++)
+				Out << CompressBuffer[CompressPosition(i) ? CompressPosition(i) - 1 : 0];
 		}
-		return 1;
+		return 0;
 		unguard;
 	}
-	UBOOL Decode( FArchive& In, FArchive& Out, FCodecNotify* Notify=NULL )
+	UBOOL Decode(FArchive& In, FArchive& Out)
 	{
 		guard(FCodecBWT::Decode);
-		TArray<BYTE> DecompressBuffer(MAX_BUFFER_SIZE+1);
-		TArray<INT>  Temp(MAX_BUFFER_SIZE+1);
-		INT DecompressLength, DecompressCount[256+1], RunningTotal[256+1], i, j;
-		while( !In.AtEnd() )
+		TArray<BYTE> DecompressBuffer(MAX_BUFFER_SIZE + 1);
+		TArray<INT>  Temp(MAX_BUFFER_SIZE + 1);
+		INT DecompressLength, DecompressCount[256 + 1], RunningTotal[256 + 1], i, j;
+		while (!In.AtEnd())
 		{
 			INT First, Last;
 			In << DecompressLength << First << Last;
-			check(DecompressLength<=MAX_BUFFER_SIZE+1);
-			check(DecompressLength<=In.TotalSize()-In.Tell());
-			In.Serialize( &DecompressBuffer(0), ++DecompressLength );
-			for( i=0; i<257; i++ )
-				DecompressCount[ i ]=0;
-			for( i=0; i<DecompressLength; i++ )
-				DecompressCount[ i!=Last ? DecompressBuffer(i) : 256 ]++;
+			check(DecompressLength <= MAX_BUFFER_SIZE + 1);
+			check(DecompressLength <= In.TotalSize() - In.Tell());
+			In.Serialize(&DecompressBuffer(0), ++DecompressLength);
+			for (i = 0; i < 257; i++)
+				DecompressCount[i] = 0;
+			for (i = 0; i < DecompressLength; i++)
+				DecompressCount[i != Last ? DecompressBuffer(i) : 256]++;
 			INT Sum = 0;
-			for( i=0; i<257; i++ )
+			for (i = 0; i < 257; i++)
 			{
 				RunningTotal[i] = Sum;
 				Sum += DecompressCount[i];
 				DecompressCount[i] = 0;
 			}
-			for( i=0; i<DecompressLength; i++ )
+			for (i = 0; i < DecompressLength; i++)
 			{
-				INT Index = i!=Last ? DecompressBuffer(i) : 256;
+				INT Index = i != Last ? DecompressBuffer(i) : 256;
 				Temp(RunningTotal[Index] + DecompressCount[Index]++) = i;
 			}
-			for( i=First,j=0 ; j<DecompressLength-1; i=Temp(i),j++ )
+			for (i = First, j = 0; j < DecompressLength - 1; i = Temp(i), j++)
 				Out << DecompressBuffer(i);
-
-			if( Notify && !Notify->NotifyProgress( (FLOAT)In.Tell() / (FLOAT)In.TotalSize() ) )
-				return 0;
 		}
 		return 1;
 		unguard;
@@ -133,59 +121,63 @@ public:
 class FCodecRLE : public FCodec
 {
 private:
-	enum {RLE_LEAD=5};
-	UBOOL EncodeEmitRun( FArchive& Out, BYTE Char, BYTE Count )
+	enum { RLE_LEAD = 5 };
+	void EncodeEmitRun(FArchive& Out, BYTE Char, BYTE Count)
 	{
-		for( INT Down=Min<INT>(Count,RLE_LEAD); Down>0; Down-- )
+		for (INT Down = Min<INT>(Count, RLE_LEAD); Down > 0; Down--)
 			Out << Char;
-		if( Count>=RLE_LEAD )
+		if (Count >= RLE_LEAD)
 			Out << Count;
-		return 1;
 	}
 public:
-	UBOOL Encode( FArchive& In, FArchive& Out, FCodecNotify* Notify=NULL )
+	FString GetCompressionName()
+	{
+		guard(FCodecRLE::GetCompressionName);
+		return TEXT("Run-Length");
+		unguard;
+	}
+
+	UBOOL Encode(FArchive& In, FArchive& Out)
 	{
 		guard(FCodecRLE::Encode);
-		BYTE PrevChar=0, PrevCount=0, B;
-		while( !In.AtEnd() )
+		BYTE PrevChar = 0, PrevCount = 0, B;
+		while (!In.AtEnd())
 		{
 			In << B;
-			if( B!=PrevChar || PrevCount==255 )
+			if (B != PrevChar || PrevCount == 255)
 			{
-				EncodeEmitRun( Out, PrevChar, PrevCount );
-				PrevChar  = B;
+				EncodeEmitRun(Out, PrevChar, PrevCount);
+				PrevChar = B;
 				PrevCount = 0;
 			}
 			PrevCount++;
 		}
-		EncodeEmitRun( Out, PrevChar, PrevCount );
-		return 1;
+		EncodeEmitRun(Out, PrevChar, PrevCount);
+		return 0;
 		unguard;
 	}
-	UBOOL Decode( FArchive& In, FArchive& Out, FCodecNotify* Notify=NULL )
+	UBOOL Decode(FArchive& In, FArchive& Out)
 	{
 		guard(FCodecRLE::Decode);
-		INT Count=0;
-		BYTE PrevChar=0, B, C;
-		while( !In.AtEnd() )
+		INT Count = 0;
+		BYTE PrevChar = 0, B, C;
+		while (!In.AtEnd())
 		{
 			In << B;
 			Out << B;
-			if( B!=PrevChar )
+			if (B != PrevChar)
 			{
 				PrevChar = B;
-				Count    = 1;
+				Count = 1;
 			}
-			else if( ++Count==RLE_LEAD )
+			else if (++Count == RLE_LEAD)
 			{
 				In << C;
-				check(C>=2);
-				while( C-->RLE_LEAD )
+				check(C >= 2);
+				while (C-- > RLE_LEAD)
 					Out << B;
 				Count = 0;
 			}
-			if( Notify && !Notify->NotifyProgress( (FLOAT)In.Tell() / (FLOAT)In.TotalSize() ) )
-				return 0;
 		}
 		return 1;
 		unguard;
@@ -204,136 +196,140 @@ private:
 		INT Ch, Count;
 		TArray<FHuffman*> Child;
 		TArray<BYTE> Bits;
-		FHuffman( INT InCh )
-		: Ch(InCh), Count(0)
+		FHuffman(INT InCh)
+			: Ch(InCh), Count(0)
 		{
 		}
 		~FHuffman()
 		{
-			for( INT i=0; i<Child.Num(); i++ )
-				delete Child( i );
+			for (INT i = 0; i < Child.Num(); i++)
+				delete Child(i);
 		}
-		void PrependBit( BYTE B )
+		void PrependBit(BYTE B)
 		{
-			Bits.Insert( 0 );
+			Bits.Insert(0);
 			Bits(0) = B;
-			for( INT i=0; i<Child.Num(); i++ )
-				Child(i)->PrependBit( B );
+			for (INT i = 0; i < Child.Num(); i++)
+				Child(i)->PrependBit(B);
 		}
-		void WriteTable( FBitWriter& Writer )
+		void WriteTable(FBitWriter& Writer)
 		{
-			Writer.WriteBit( Child.Num()!=0 );
-			if( Child.Num() )
-				for( INT i=0; i<Child.Num(); i++ )
-					Child(i)->WriteTable( Writer );
+			Writer.WriteBit(Child.Num() != 0);
+			if (Child.Num())
+				for (INT i = 0; i < Child.Num(); i++)
+					Child(i)->WriteTable(Writer);
 			else
 			{
 				BYTE B = Ch;
 				Writer << B;
 			}
 		}
-		void ReadTable( FBitReader& Reader )
+		void ReadTable(FBitReader& Reader)
 		{
-			if( Reader.ReadBit() )
+			if (Reader.ReadBit())
 			{
-				Child.Add( 2 );
-				for( INT i=0; i<Child.Num(); i++ )
+				Child.Add(2);
+				for (INT i = 0; i < Child.Num(); i++)
 				{
-					Child( i ) = new FHuffman( -1 );
-					Child( i )->ReadTable( Reader );
+					Child(i) = new FHuffman(-1);
+					Child(i)->ReadTable(Reader);
 				}
 			}
-			else Ch = Arctor<BYTE>( Reader );
+			else Ch = Arctor<BYTE>(Reader);
 		}
 	};
-	static QSORT_RETURN CDECL CompareHuffman( const FHuffman** A, const FHuffman** B )
+	static QSORT_RETURN CDECL CompareHuffman(const FHuffman** A, const FHuffman** B)
 	{
 		return (*B)->Count - (*A)->Count;
 	}
 public:
-	UBOOL Encode( FArchive& In, FArchive& Out, FCodecNotify* Notify=NULL )
+	FString GetCompressionName()
+	{
+		guard(FCodecHuffman::GetCompressionName);
+		return TEXT("Huffman Coding");
+		unguard;
+	}
+
+	UBOOL Encode(FArchive& In, FArchive& Out)
 	{
 		guard(FCodecHuffman::Encode);
 		INT SavedPos = In.Tell();
-		INT Total=0, i;
+		INT Total = 0, i;
 
 		// Compute character frequencies.
 		TArray<FHuffman*> Huff(256);
-		for( i=0; i<256; i++ )
+		for (i = 0; i < 256; i++)
 			Huff(i) = new FHuffman(i);
 		TArray<FHuffman*> Index = Huff;
-		while( !In.AtEnd() )
+		while (!In.AtEnd())
 			Huff(Arctor<BYTE>(In))->Count++, Total++;
-		In.Seek( SavedPos );
+		In.Seek(SavedPos);
 		Out << Total;
 
 		// Build compression table.
-		while( Huff.Num()>1 && Huff.Last()->Count==0 )
+		while (Huff.Num() > 1 && Huff.Last()->Count == 0)
 			delete Huff.Pop();
-		INT BitCount = Huff.Num()*(8+1);
-		while( Huff.Num()>1 )
+		INT BitCount = Huff.Num() * (8 + 1);
+		while (Huff.Num() > 1)
 		{
-			FHuffman* Node  = new FHuffman( -1 );
-			Node->Child.Add( 2 );
-			for( i=0; i<Node->Child.Num(); i++ )
+			FHuffman* Node = new FHuffman(-1);
+			Node->Child.Add(2);
+			for (i = 0; i < Node->Child.Num(); i++)
 			{
 				Node->Child(i) = Huff.Pop();
 				Node->Child(i)->PrependBit(i);
 				Node->Count += Node->Child(i)->Count;
 			}
-			for( i=0; i<Huff.Num(); i++ )
-				if( Huff(i)->Count < Node->Count )
+			for (i = 0; i < Huff.Num(); i++)
+				if (Huff(i)->Count < Node->Count)
 					break;
-			Huff.Insert( i );
-			Huff( i ) = Node;
+			Huff.Insert(i);
+			Huff(i) = Node;
 			BitCount++;
 		}
 		FHuffman* Root = Huff.Pop();
 
 		// Calc stats.
-		while( !In.AtEnd() )
+		while (!In.AtEnd())
 			BitCount += Index(Arctor<BYTE>(In))->Bits.Num();
-		In.Seek( SavedPos );
+		In.Seek(SavedPos);
 
 		// Save table and bitstream.
-		FBitWriter Writer( BitCount );
-		Root->WriteTable( Writer );
-		while( !In.AtEnd() )
+		FBitWriter Writer(BitCount);
+		Root->WriteTable(Writer);
+		while (!In.AtEnd())
 		{
 			FHuffman* P = Index(Arctor<BYTE>(In));
-			for( INT j=0; j<P->Bits.Num(); j++ )
-				Writer.WriteBit( P->Bits(j) );
+			for (INT j = 0; j < P->Bits.Num(); j++)
+				Writer.WriteBit(P->Bits(j));
 		}
 		check(!Writer.IsError());
-		check(Writer.GetNumBits()==BitCount);
-		Out.Serialize( Writer.GetData(), Writer.GetNumBytes() );
+		check(Writer.GetNumBits() == BitCount);
+		Out.Serialize(Writer.GetData(), Writer.GetNumBytes());
 
 		// Finish up.
 		delete Root;
-		return 1;
-
+		return 0;
 		unguard;
 	}
-	UBOOL Decode( FArchive& In, FArchive& Out, FCodecNotify* Notify=NULL )
+	UBOOL Decode(FArchive& In, FArchive& Out)
 	{
 		guard(FCodecHuffman::Decode);
-		INT Total, Remaining;
+		INT Total;
 		In << Total;
-		Remaining = Total;
-		TArray<BYTE> InArray( In.TotalSize()-In.Tell() );
-		In.Serialize( &InArray(0), InArray.Num() );
-		FBitReader Reader( &InArray(0), InArray.Num()*8 );
+		TArray<BYTE> InArray(In.TotalSize() - In.Tell());
+		In.Serialize(&InArray(0), InArray.Num());
+		FBitReader Reader(&InArray(0), InArray.Num() * 8);
 		FHuffman Root(-1);
-		Root.ReadTable( Reader );
-		while( Remaining-- > 0 )
+		Root.ReadTable(Reader);
+		while (Total-- > 0)
 		{
 			check(!Reader.AtEnd());
-			FHuffman* Node;
-			for( Node=&Root; Node->Ch==-1; Node=Node->Child(Reader.ReadBit()) );
+			FHuffman* Node = &Root;
+			while (Node->Ch == -1)
+				Node = Node->Child(Reader.ReadBit());
 			BYTE B = Node->Ch;
 			Out << B;
-			if( Notify && !Notify->NotifyProgress( (FLOAT)(Total-Remaining) / (FLOAT)Total ) )
-				return 0;
 		}
 		return 1;
 		unguard;
@@ -347,48 +343,53 @@ public:
 class FCodecMTF : public FCodec
 {
 public:
-	UBOOL Encode( FArchive& In, FArchive& Out, FCodecNotify* Notify=NULL )
+	FString GetCompressionName()
+	{
+		guard(FCodecMTF::GetCompressionName);
+		return TEXT("Move-To-Front");
+		unguard;
+	}
+
+	UBOOL Encode(FArchive& In, FArchive& Out)
 	{
 		guard(FCodecMTF::Encode);
 		BYTE List[256], B, C;
 		INT i;
-		for( i=0; i<256; i++ )
+		for (i = 0; i < 256; i++)
 			List[i] = i;
-		while( !In.AtEnd() )
+		while (!In.AtEnd())
 		{
 			In << B;
-			for( i=0; i<256; i++ )
-				if( List[i]==B )
+			for (i = 0; i < 256; i++)
+				if (List[i] == B)
 					break;
-			check(i<256);
+			check(i < 256);
 			C = i;
 			Out << C;
-			INT NewPos=0;
-			for( ; i>NewPos; i-- )
-				List[i]=List[i-1];
+			INT NewPos = 0;
+			for (; i > NewPos; i--)
+				List[i] = List[i - 1];
 			List[NewPos] = B;
 		}
-		return 1;
+		return 0;
 		unguard;
 	}
-	UBOOL Decode( FArchive& In, FArchive& Out, FCodecNotify* Notify=NULL )
+	UBOOL Decode(FArchive& In, FArchive& Out)
 	{
 		guard(FCodecMTF::Decode);
 		BYTE List[256], B, C;
 		INT i;
-		for( i=0; i<256; i++ )
+		for (i = 0; i < 256; i++)
 			List[i] = i;
-		while( !In.AtEnd() )
+		while (!In.AtEnd())
 		{
 			In << B;
 			C = List[B];
 			Out << C;
-			INT NewPos=0;
-			for( i=B; i>NewPos; i-- )
-				List[i]=List[i-1];
+			INT NewPos = 0;
+			for (i = B; i > NewPos; i--)
+				List[i] = List[i - 1];
 			List[NewPos] = C;
-			if( Notify && !Notify->NotifyProgress( (FLOAT)In.Tell() / (FLOAT)In.TotalSize() ) )
-				return 0;
 		}
 		return 1;
 		unguard;
@@ -399,81 +400,70 @@ public:
 	General compressor codec.
 -----------------------------------------------------------------------------*/
 
-class FCodecFull : public FCodec, FCodecNotify
+class FCodecFull : public FCodec
 {
 private:
 	TArray<FCodec*> Codecs;
-	INT Stage;
-	FCodecNotify* Notify;
-	UBOOL NotifyProgress( FLOAT Progress )
-	{
-		if( Notify && !Notify->NotifyProgress( ((FLOAT)Stage / (FLOAT)Codecs.Num()) + (Progress / (FLOAT)Codecs.Num()) ) )
-			return 0;
-		return 1;
-	}
-	UBOOL Code( FArchive& In, FArchive& Out, INT Step, INT First, UBOOL (FCodec::*Func)(FArchive&,FArchive&,FCodecNotify*), FCodecNotify* InNotify=NULL )
+	void Code(FArchive& In, FArchive& Out, INT Step, INT First, UBOOL(FCodec::* Func)(FArchive&, FArchive&))
 	{
 		guard(FCodecFull::Code);
 		TArray<BYTE> InData, OutData;
-		FLOAT TotalTime=0.f;
-		Notify = InNotify;
-		FOutputDevice* LogOut = (GIsEditor ? GWarn : GLog);
-		for( INT i=0; i<Codecs.Num(); i++ )
+		FLOAT TotalTime = 0.f;
+		for (INT i = 0; i < Codecs.Num(); i++)
 		{
+			FString CompName = Codecs(First + Step * i)->GetCompressionName();
 			FBufferReader Reader(InData);
 			FBufferWriter Writer(OutData);
 			FTime StartTime, EndTime;
 			StartTime = appSeconds();
-			Stage = i;
-			if( !(Codecs(First + Step*i)->*Func)( *(i ? &Reader : &In), *(i<Codecs.Num()-1 ? &Writer : &Out), this ) )
-				return 0;
+			(Codecs(First + Step * i)->*Func)(*(i ? &Reader : &In), *(i < Codecs.Num() - 1 ? &Writer : &Out));
 			EndTime = appSeconds() - StartTime;
 			TotalTime += EndTime.GetFloat();
-			LogOut->Logf(NAME_Progress, TEXT("stage %d: %f secs"), i, EndTime.GetFloat() );
-			if( i<Codecs.Num()-1 )
+			GWarn->Logf(NAME_Heading, TEXT("%s: %f secs"), *CompName, EndTime.GetFloat());
+			if (i < Codecs.Num() - 1)
 			{
 				InData = OutData;
 				OutData.Empty();
 			}
 		}
-		LogOut->Logf(NAME_Progress, TEXT("Total: %f secs"), TotalTime );
-		return 1;
+		GWarn->Logf(NAME_Heading, TEXT("Total: %f secs"), TotalTime);
 		unguard;
 	}
 public:
-	UBOOL Encode( FArchive& In, FArchive& Out, FCodecNotify* InNotify=NULL )
+	FString GetCompressionName()
+	{
+		guard(FCodecFull::GetCompressionName);
+		return TEXT("Full Codec");
+		unguard;
+	}
+
+	UBOOL Encode(FArchive& In, FArchive& Out)
 	{
 		guard(FCodecFull::Encode);
-		return Code( In, Out, 1, 0, &FCodec::Encode, InNotify );
+		Code(In, Out, 1, 0, &FCodec::Encode);
+		return 0;
 		unguard;
 	}
-	UBOOL Decode( FArchive& In, FArchive& Out, FCodecNotify* InNotify=NULL )
+	UBOOL Decode(FArchive& In, FArchive& Out)
 	{
 		guard(FCodecFull::Decode);
-		return Code( In, Out, -1, Codecs.Num()-1, &FCodec::Decode, InNotify );
+		Code(In, Out, -1, Codecs.Num() - 1, &FCodec::Decode);
+		return 1;
 		unguard;
 	}
-	void AddCodec( FCodec* InCodec )
+	void AddCodec(FCodec* InCodec)
 	{
 		guard(FCodecFull::AddCodec);
-		Codecs.AddItem( InCodec );
+		Codecs.AddItem(InCodec);
 		unguard;
 	}
-	~FCodecFull() noexcept(false)
+	~FCodecFull()
 	{
 		guard(FCodecFull::~FCodecFull);
-		for( INT i=0; i<Codecs.Num(); i++ )
-			delete Codecs( i );
+		for (INT i = 0; i < Codecs.Num(); i++)
+			delete Codecs(i);
 		unguard;
 	}
 };
-
-#if ((_MSC_VER) || (HAVE_PRAGMA_PACK))
-#pragma pack (pop)
 #endif
 
-#endif
-
-/*-----------------------------------------------------------------------------
-	The End.
------------------------------------------------------------------------------*/
